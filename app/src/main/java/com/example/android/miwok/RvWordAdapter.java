56130b2d505a348004efecb1d9024527d078f52d
mnovillodiaz <mnovillodiaz@gmail.com>
2017-06-04 16:54:09 +0200

    Cleaning animation part

--- app/src/main/java/com/example/android/miwok/RvWordAdapter.java
+++ app/src/main/java/com/example/android/miwok/RvWordAdapter.java
@@ -23,23 +23,6 @@ public class RvWordAdapter extends RecyclerView.Adapter<RvWordAdapter.ViewHolder
     private Context mContext;
     private int lastPosition = -1;
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        public ImageView mImageView;
-        public TextView mdefaultText;
-        public TextView mMiwokText;
-        public View mTextContainer;
-        public View mWordGroup;
-
-        public ViewHolder(View itemView) {
-            super(itemView);
-            mImageView = (ImageView) itemView.findViewById(R.id.image);
-            mdefaultText = (TextView) itemView.findViewById(R.id.defaultText);
-            mMiwokText = (TextView) itemView.findViewById(R.id.miwokText);
-            mTextContainer = itemView.findViewById(R.id.text_container);
-            mWordGroup = itemView.findViewById(R.id.word_group);
-        }
-    }
-
     //constructor
     public RvWordAdapter(Context context, ArrayList<Word> words, int ColorReourceId) {
         mContext = context;
@@ -63,6 +46,7 @@ public class RvWordAdapter extends RecyclerView.Adapter<RvWordAdapter.ViewHolder
         ViewHolder viewHolder = new ViewHolder(wordView);
         return viewHolder;
     }
+
     // Involves populating data into the item through holder
     @Override
     public void onBindViewHolder(RvWordAdapter.ViewHolder viewHolder, int position) {
@@ -86,7 +70,7 @@ public class RvWordAdapter extends RecyclerView.Adapter<RvWordAdapter.ViewHolder
         TextView textViewMiwok = viewHolder.mMiwokText;
         textViewMiwok.setText(word.getMiwokTranslation());
 
-        // Find the textContainer to be able to set the color
+        // Find the textContainer to be able to set the color depending in position
         View TextContainer = viewHolder.mTextContainer;
         if(position % 2 == 0){
             TextContainer.setBackgroundResource(R.color.tan_background);
@@ -97,25 +81,38 @@ public class RvWordAdapter extends RecyclerView.Adapter<RvWordAdapter.ViewHolder
             TextContainer.setBackgroundResource(mColorResourceId);
         }
 
-        setAnimation(viewHolder.mWordGroup,position);
+        //Applying a basic animation to the word group
+        setAnimation(viewHolder.mWordGroup);
 
     }
 
-    private void setAnimation(View viewToAnimate, int position)
-    {
-/*        // If the bound view wasn't previously displayed on screen, it's animated
-        if (position > lastPosition)
-        {*/
+    // Basic Fade in Animation
+    private void setAnimation(View viewToAnimate) {
             Animation animation = AnimationUtils.loadAnimation(mContext, android.R.anim.fade_in);
             viewToAnimate.startAnimation(animation);
-           /* lastPosition = position;
-        }*/
     }
 
     // Returns the total count of items in the list
     @Override
     public int getItemCount() {
         return mWords.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView mImageView;
+        public TextView mdefaultText;
+        public TextView mMiwokText;
+        public View mTextContainer;
+        public View mWordGroup;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mImageView = (ImageView) itemView.findViewById(R.id.image);
+            mdefaultText = (TextView) itemView.findViewById(R.id.defaultText);
+            mMiwokText = (TextView) itemView.findViewById(R.id.miwokText);
+            mTextContainer = itemView.findViewById(R.id.text_container);
+            mWordGroup = itemView.findViewById(R.id.word_group);
+        }
     }
 }
 
