b5876858376fefa873b1f6047e112ca7ddb3d927
mdiaz <mdiaz@zalando-25685.localdomain>
2017-07-02 19:04:37 +0200

    removing TabLayout, adding menu screen, OnClickListeners for options

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -16,8 +16,6 @@
 package com.example.android.miwok;
 
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,16 +27,5 @@ public class MainActivity extends AppCompatActivity {
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
-        // Find the view pager that will allow the user to swipe between fragments
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-
-        // Create an adapter that knows which fragment should be shown on each page
-        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
-
-        // Set the adapter onto the view pager
-        viewPager.setAdapter(adapter);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
-        tabLayout.setupWithViewPager(viewPager);
     }
 }
---
+++ app/src/main/java/com/example/android/miwok/OptionsFragment.java
@@ -0,0 +1,63 @@
+package com.example.android.miwok;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by mdiaz on 02.07.17.
+ */
+
+public class OptionsFragment  extends Fragment implements View.OnClickListener {
+
+    public OptionsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.options, container, false);
+        TextView numbersTextView = (TextView) rootView.findViewById(R.id.numbers_category);
+        TextView familyTextView = (TextView) rootView.findViewById(R.id.family_category);
+        TextView colorsTextView = (TextView) rootView.findViewById(R.id.colors_category);
+        TextView phrasesTextView = (TextView) rootView.findViewById(R.id.phrases_category);
+
+        numbersTextView.setOnClickListener(this);
+        familyTextView.setOnClickListener(this);
+        colorsTextView.setOnClickListener(this);
+        phrasesTextView.setOnClickListener(this);
+
+        return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.numbers_category:
+                goToFragment(new NumbersFragment());
+                break;
+            case R.id.family_category:
+                goToFragment(new FamilyFragment());
+                break;
+            case R.id.colors_category:
+                goToFragment(new ColorsFragment());
+                break;
+            case R.id.phrases_category:
+                goToFragment(new PhrasesFragment());
+                break;
+        }
+    }
+
+    private void goToFragment(Fragment frag){
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.mainFrameLayout, frag);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
